77cfcc27239870997bb453a55eec4494c32087a1
lsy <598348605@qq.com>
2020-08-17 18:34:51 +0800

    Format the code

--- src/page/h5Store/storeGoods/components/selectSpec.vue
+++ src/page/h5Store/storeGoods/components/selectSpec.vue
@@ -1,82 +1,92 @@
 <template>
-    <div :class="$style.select_goods">
-        <!-- 选择商品弹窗 -->
-        <el-dialog
-            :title="$t('selectGoods')"
-            width="80%"
-            min-width="1000px"
-            :visible.sync="visible"
-            :before-close="handleClose">
-            <el-row :gutter="20" style="line-height:30px;">
-              <el-col :span="5">{{$t('PleaseSelectCategory')}}</el-col>
-              <el-col :span="19">
-                  <!-- 搜索框 -->
-                  <el-row type="flex">
-                    <el-col  :span="16">
-                        <el-input
-                            clearable
-                            @clear="hanlderClearKeywords"
-                            :placeholder="$t('PleaseEnterAKeyword')"
-                            v-model="keywords">
-                            <i slot="prefix" class="el-input__icon el-icon-search"></i>
-                        </el-input>
-                    </el-col>
-                    <!-- 添加商品 -->
-                    <el-col :span="4" :offset="4" >
-                      <div style="float:right">
-                      <el-button :loading="isButtonLoading" @click="goToAddGoodPage" icon="el-icon-document-add"> {{$t('AddProduct')}} </el-button>
-                      </div>
-                    </el-col>
-                  </el-row>
-              </el-col>
-            </el-row>
-            <hr/>
-            <el-row :gutter="20">
-              <el-col :span="5" >
-                  <span style="cursor: pointer;" @click="handlerAllCatergroy">{{$t('AllCartory')}}</span>
-                  <el-tree
-                    :props="{label:'name_cn'}"
-                    :data="categoryListData"
-                    default-expand-all
-                    @node-click="onCategoryChange"
-                    :expand-on-click-node="false">
-                  </el-tree>
-              </el-col>
-              <el-col :span="19"  style="border-left:1px solid #ccc">
-                  <!-- 数据表 -->
-                  <el-table
-                      :data="specList"
-                      ref="table"
-                      border
-                      @selection-change="specRowChange" style="width: 100%; margin-top:10px;">
-                      <el-table-column type="selection" width="60" header-align="center" align="center"></el-table-column>
-                      <el-table-column type="index" label="#" width="60" header-align="center" align="center"></el-table-column>
-                      <el-table-column  :label="$t('ProductName2')" prop="name_cn" header-align="center" align="center"></el-table-column>
-                      <el-table-column  :label="$t('CurrentStock')" prop="total_stock_num" align="center"></el-table-column>
-                  </el-table>
-                  <button-pagination :pageParams="params" @changePage="handleCurrentChange"></button-pagination>
-              </el-col>
-            </el-row>
-            <hr/>
-            <el-row :gutter="20">
-              <el-col :span="5">
-                {{$t('productionType')}}{{specSelected.length}}
-              </el-col>
-              <el-col :span="19" class="dialog-footer">
-                <div style="float:right">
-                  <el-button
-                      type="primary"
-                      @click="confirmSelected"
-                      :loading="isButtonLoading"
-                      :disabled="!this.specSelected.length">
-                      {{$t('submit')}}
-                  </el-button>
-                  <el-button :loading="isButtonLoading" @click="handleClose()" >{{$t('cancel')}}</el-button>
-                </div>
-              </el-col>
-            </el-row>
-        </el-dialog>
-    </div>
+  <div :class="$style.select_goods">
+    <!-- 选择商品弹窗 -->
+    <el-dialog
+      :title="$t('selectGoods')"
+      width="80%"
+      min-width="1000px"
+      :visible.sync="visible"
+      :before-close="handleClose"
+    >
+      <el-row :gutter="20" style="line-height:30px;">
+        <el-col :span="5">{{$t('PleaseSelectCategory')}}</el-col>
+        <el-col :span="19">
+          <!-- 搜索框 -->
+          <el-row type="flex">
+            <el-col :span="16">
+              <el-input
+                clearable
+                @clear="hanlderClearKeywords"
+                :placeholder="$t('PleaseEnterAKeyword')"
+                v-model="keywords"
+              >
+                <i slot="prefix" class="el-input__icon el-icon-search"></i>
+              </el-input>
+            </el-col>
+            <!-- 添加商品 -->
+            <el-col :span="4" :offset="4">
+              <div style="float:right">
+                <el-button
+                  :loading="isButtonLoading"
+                  @click="goToAddGoodPage"
+                  icon="el-icon-document-add"
+                >{{$t('AddProduct')}}</el-button>
+              </div>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+      <hr />
+      <el-row :gutter="20">
+        <el-col :span="5">
+          <span style="cursor: pointer;" @click="handlerAllCatergroy">{{$t('AllCartory')}}</span>
+          <el-tree
+            :props="{label:'name_cn'}"
+            :data="categoryListData"
+            default-expand-all
+            @node-click="onCategoryChange"
+            :expand-on-click-node="false"
+          ></el-tree>
+        </el-col>
+        <el-col :span="19" style="border-left:1px solid #ccc">
+          <!-- 数据表 -->
+          <el-table
+            :data="specList"
+            ref="table"
+            border
+            @selection-change="specRowChange"
+            style="width: 100%; margin-top:10px;"
+          >
+            <el-table-column type="selection" width="60" header-align="center" align="center"></el-table-column>
+            <el-table-column type="index" label="#" width="60" header-align="center" align="center"></el-table-column>
+            <el-table-column
+              :label="$t('ProductName2')"
+              prop="name_cn"
+              header-align="center"
+              align="center"
+            ></el-table-column>
+            <el-table-column :label="$t('CurrentStock')" prop="total_stock_num" align="center"></el-table-column>
+          </el-table>
+          <button-pagination :pageParams="params" @changePage="handleCurrentChange"></button-pagination>
+        </el-col>
+      </el-row>
+      <hr />
+      <el-row :gutter="20">
+        <el-col :span="5">{{$t('productionType')}}{{specSelected.length}}</el-col>
+        <el-col :span="19" class="dialog-footer">
+          <div style="float:right">
+            <el-button
+              type="primary"
+              @click="confirmSelected"
+              :loading="isButtonLoading"
+              :disabled="!this.specSelected.length"
+            >{{$t('submit')}}</el-button>
+            <el-button :loading="isButtonLoading" @click="handleClose()">{{$t('cancel')}}</el-button>
+          </div>
+        </el-col>
+      </el-row>
+    </el-dialog>
+  </div>
 </template>
 <script>
 import inputPublic from '@/components/input-public';
@@ -220,7 +230,6 @@ export default {
 </script>
 
 <style lang="less" module>
-
 .select_goods {
   .select_goods_btn {
     margin: 0 0 10px 0;
