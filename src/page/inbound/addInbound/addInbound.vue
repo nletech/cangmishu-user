4e762ea098ab0fd4789d2a43fcfbd5e0389ebc9f
胡斌杰 <hubinjie@live.cn>
2021-01-18 16:31:03 +0800

    入库单详细优化完成

--- src/page/inbound/addInbound/addInbound.vue
+++ src/page/inbound/addInbound/addInbound.vue
@@ -2,11 +2,11 @@
   <div class="storeManage">
     <mdoel-form :colValue="24">
       <el-form slot="left" :rules="rules" :model="form" label-width="100px" ref="form">
+        <h2 align="center" style="margin:0px;">
+          {{ $t('addInbound') }}
+        </h2>
+        <hr />
         <el-row type="flex" justify="space-between">
-          <el-col :span="8"> </el-col>
-          <el-col :span="7"
-            ><h2 align="center" style="margin:0px;">{{ $t('addInbound') }}</h2></el-col
-          >
           <el-col :span="8">
             <el-form-item
               :label="$t('Numbers')"
@@ -14,16 +14,15 @@
               prop="confirmation_number"
               class="noborder"
             >
-              <el-input v-model="form.confirmation_number" prefix-icon="el-icon-tickets">
+              <el-input
+                v-model="form.confirmation_number"
+                prefix-icon="el-icon-tickets"
+                style="width:250px;"
+              >
                 <el-button slot="append" @click="getBatchCode" icon="el-icon-refresh"></el-button>
               </el-input>
             </el-form-item>
           </el-col>
-        </el-row>
-        <hr />
-        <label class="label">{{ $t('Essentialformrmation') }}</label>
-        <!-- 入库单分类 -->
-        <el-row type="flex" justify="space-between">
           <el-col :span="8">
             <el-form-item :label="$t('category')" label-position="right" prop="type_id">
               <el-select v-model="form.type_id">
@@ -38,8 +37,7 @@
               </el-select>
             </el-form-item>
           </el-col>
-          <el-col :span="5"> </el-col>
-          <el-col :span="10">
+          <el-col :span="8">
             <!-- 供应商 -->
             <el-form-item
               :label="$t('supplier')"
@@ -60,142 +58,160 @@
             </el-form-item>
           </el-col>
         </el-row>
-        <el-row>
-          <el-col>
-            <label class="label" style="float:left; width:80px;">{{ $t('goodsList') }} </label>
-            <div style="float:left; width:300px; padding-top:20px">
-              <el-button size="mini" @click="showDialog" type="primary" plain
+        <hr />
+        <!-- 入库单分类 -->
+        <!-- 选择商品的列表 -->
+        <el-card class="box-card">
+          <el-row>
+            <el-col>
+              <el-input
+                style="width:300px"
+                placeholder="请扫码条码或SKU编号"
+                size="mini"
+                v-model="barcode"
+                @keyup.enter.native="onBarcodeSubmit"
+              >
+                <el-button slot="append" icon="el-icon-search"></el-button>
+              </el-input>
+              <!-- 多项弹窗 -->
+              <el-dialog title="请选择商品规格" :visible.sync="dialogSpecVisible">
+                <el-alert title="存在多条记录,请选择一条" type="warning" show-icon></el-alert>
+                <el-table :data="barcodeSearchSpecsResult" @row-click="handlePopoverSelectClient">
+                  <el-table-column
+                    :label="$t('ProductSpec')"
+                    prop="product_name"
+                    header-align="center"
+                    align="center"
+                  >
+                  </el-table-column>
+                  <el-table-column
+                    :label="$t('PurchasePrice')"
+                    prop="purchase_price"
+                    align="center"
+                  ></el-table-column>
+                  <el-table-column
+                    :label="$t('SalePrice')"
+                    prop="sale_price"
+                    align="center"
+                  ></el-table-column>
+                  <el-table-column
+                    :label="$t('Stock')"
+                    prop="total_stock_num"
+                    align="center"
+                  ></el-table-column>
+                  <el-table-column label="操作">
+                    <template slot-scope="scope">
+                      <el-button size="mini" @click="confirmBarcodeSubmit(scope.row)"
+                        >确定</el-button
+                      >
+                    </template>
+                  </el-table-column>
+                </el-table>
+              </el-dialog>
+              <el-button size="mini" @click="showDialog" type="primary" style="float:right" plain
                 >{{ $t('pleaseSelect') }}<i class="el-icon-more el-icon--right"></i>
               </el-button>
-              <span class="sub-title">{{ $t('mustEnter') }}</span>
-            </div>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col>
-            <el-table :data="specList" :empty-text="$t('pleaseSelect')">
-              <el-table-column type="index" label="#" fixed> </el-table-column>
-              <el-table-column
-                :label="$t('specName')"
-                prop="product_name"
-                width="200px"
-                fixed
-              ></el-table-column>
-              <el-table-column label="SKU" prop="relevance_code" width="150px"></el-table-column>
-              <el-table-column :label="$t('inboundNumbers')" width="150px;">
-                <template slot-scope="scope">
-                  <el-input-number size="mini" v-model="scope.row.need_num" :min="1">
-                  </el-input-number>
-                </template>
-              </el-table-column>
-              <el-table-column :label="$t('PurcharsePrice')" min-width="80px">
-                <template slot-scope="scope">
-                  <el-input size="mini" v-model="scope.row.purchase_price"> </el-input>
-                </template>
-              </el-table-column>
-              <el-table-column :label="$t('remark')">
-                <template slot-scope="scope">
-                  <el-input size="mini" v-model="scope.row.remark"></el-input>
-                </template>
-              </el-table-column>
-              <el-table-column min-width="60px">
-                <template slot-scope="scope">
-                  <el-tooltip :content="$t('delete')" placement="top">
-                    <el-button
-                      size="mini"
-                      icon="el-icon-delete"
-                      @click="removeGoods(scope.$index)"
-                      type="danger"
-                      round
-                    >
-                    </el-button>
-                  </el-tooltip>
-                </template>
-              </el-table-column>
-            </el-table>
-          </el-col>
-        </el-row>
-        <!-- 运输方式 -->
-        <label class="label"> {{ $t('notNecessaryInfo') }} </label>
-        <el-row>
-          <el-col>
+              <div style="clear:both; height:10px;"></div>
+              <el-table
+                :data="specList"
+                :empty-text="$t('pleaseSelect')"
+                border
+                show-summary
+                :summary-method="getSummaries"
+              >
+                <el-table-column type="index" label="#" fixed width="100"> </el-table-column>
+                <el-table-column
+                  :label="$t('specName')"
+                  prop="product_name"
+                  width="200px"
+                  fixed
+                ></el-table-column>
+                <el-table-column label="SKU" prop="relevance_code" width="150px"></el-table-column>
+                <el-table-column :label="$t('inboundNumbers')" width="180px;">
+                  <template slot-scope="scope">
+                    <el-input-number size="mini" v-model="scope.row.need_num" :min="1">
+                    </el-input-number>
+                  </template>
+                </el-table-column>
+                <el-table-column :label="$t('PurcharsePrice')" min-width="80px">
+                  <template slot-scope="scope">
+                    <el-input size="mini" v-model="scope.row.purchase_price"> </el-input>
+                  </template>
+                </el-table-column>
+                <el-table-column label="金额" width="120px" align="center">
+                  <template slot-scope="scope">
+                    {{ parseFloat(scope.row.purchase_price * scope.row.need_num).toFixed(2) }}
+                  </template>
+                </el-table-column>
+                <el-table-column :label="$t('remark')">
+                  <template slot-scope="scope">
+                    <el-input size="mini" v-model="scope.row.remark"></el-input>
+                  </template>
+                </el-table-column>
+                <el-table-column min-width="60px">
+                  <template slot-scope="scope">
+                    <el-tooltip :content="$t('delete')" placement="top">
+                      <el-button
+                        size="mini"
+                        icon="el-icon-delete"
+                        @click="removeGoods(scope.$index)"
+                        type="danger"
+                        round
+                      >
+                      </el-button>
+                    </el-tooltip>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </el-col>
+          </el-row>
+        </el-card>
+        <hr />
+        <!-- 提交按钮 -->
+        <el-row :gutter="24">
+          <el-col :span="18">
             <el-form-item :label="$t('planInboundTime')">
-              <el-row>
-                <el-col :span="12" style="padding:0;">
-                  <el-date-picker
-                    v-model="startDate"
-                    type="date"
-                    :placeholder="$t('startDate')"
-                    :picker-options="pickerOptions"
-                    value-format="yyyy-MM-dd"
-                    size="small"
-                  >
-                  </el-date-picker>
-                </el-col>
-                <el-col :span="5" :offset="1">
-                  <el-time-select
-                    v-model="startTime"
-                    :picker-options="{
-                      start: '00:00',
-                      step: '00:30',
-                      end: '23:30'
-                    }"
-                    :placeholder="$t('startTime')"
-                    size="small"
-                  >
-                  </el-time-select>
-                </el-col>
-              </el-row>
-              <el-row>
-                <el-col :span="12">
-                  <el-date-picker
-                    v-model="endDate"
-                    type="date"
-                    :placeholder="$t('endDate')"
-                    :picker-options="pickerOptions"
-                    value-format="yyyy-MM-dd"
-                    size="small"
-                  >
-                  </el-date-picker>
-                </el-col>
-                <el-col :span="5" :offset="1">
-                  <el-time-select
-                    v-model="endTime"
-                    :picker-options="{
-                      start: '00:00',
-                      step: '00:30',
-                      end: '23:30'
-                    }"
-                    :placeholder="$t('endTime')"
-                    size="small"
-                  >
-                  </el-time-select>
-                </el-col>
-              </el-row>
+              <el-date-picker
+                v-model="startDate"
+                type="date"
+                :placeholder="$t('startDate')"
+                :picker-options="pickerOptions"
+                value-format="yyyy-MM-dd"
+                size="small"
+              >
+              </el-date-picker>
             </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col>
             <el-form-item :label="$t('remark')">
               <el-input
                 v-model="form.remark"
                 type="textarea"
+                rows="6"
                 :placeholder="$t('TheFieldmaynotbegreaterthan30characters')"
                 :maxlength="30"
               >
               </el-input>
             </el-form-item>
           </el-col>
+          <el-col :span="6">
+            <el-form-item label="商品数量:">
+              <span style="font-size:18px; font-weight:bold">{{ form.sub_qty }}</span>
+            </el-form-item>
+            <el-divider></el-divider>
+            <el-form-item label="总金额:">
+              <span style="color:red; font-size:18px; font-weight:bold">{{
+                form.sub_total.toFixed(2)
+              }}</span>
+            </el-form-item>
+            <el-button
+              @click="onSave('form')"
+              type="primary"
+              :loading="isButtonLoading"
+              style="width:100%;margin-bottom:15px;height:50px;"
+            >
+              {{ $t('submit') }}
+            </el-button>
+          </el-col>
         </el-row>
-        <el-form-item style="padding-left:100px;">
-          <el-button @click="onSave('form')" type="primary" :loading="isButtonLoading">
-            {{ $t('submit') }}
-          </el-button>
-          <el-button @click="$router.go(-1)">
-            {{ $t('cancel') }}
-          </el-button>
-        </el-form-item>
       </el-form>
     </mdoel-form>
     <!-- 选择商品弹窗 -->
@@ -228,12 +244,6 @@
             align="center"
           >
           </el-table-column>
-          <!-- <el-table-column
-                        prop="name_en"
-                        label="供应商外文名称"
-                        header-align="center"
-                        align="center">
-                    </el-table-column> -->
           <el-table-column :label="$t('operation')" width="200" header-align="center">
             <template slot-scope="scope">
               <!-- 编辑 -->
@@ -306,7 +316,9 @@ export default {
         distributor_id: '',
         remarks: '',
         transportation_type: '',
-        waybill_number: ''
+        waybill_number: '',
+        sub_qty: 0,
+        sub_total: 0
       },
       startDate: '',
       startTime: '',
@@ -325,6 +337,8 @@ export default {
       distributor: {
         page: 0
       },
+      dialogSpecVisible: false,
+      barcode: '',
       distributorSelectList: [], // 供应商列表
       rules: {
         confirmation_number: [
@@ -349,6 +363,71 @@ export default {
     }
   },
   methods: {
+    getSummaries(param) {
+      const { columns, data } = param;
+      const sums = [];
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = '合计';
+          return;
+        }
+        const values = data.map(item => Number(item[column.property]));
+        if (index === 3 && !values.every(value => isNaN(value))) {
+          sums[index] = values.reduce((prev, curr) => {
+            const value = Number(curr);
+            if (!isNaN(value)) {
+              return prev + curr;
+            } else {
+              return prev;
+            }
+          }, 0);
+        }
+        if (index === 5) {
+          let getTotal = 0;
+          let getQty = 0;
+          console.log(getTotal);
+          this.specList.forEach(item => {
+            getQty += item.need_num;
+            getTotal += item.need_num * item.purchase_price;
+          });
+          sums[index] = Number(getTotal).toFixed(2);
+          this.form.sub_qty = getQty;
+          this.form.sub_total = getTotal;
+        }
+      });
+
+      return sums;
+    },
+    confirmBarcodeSubmit(resSpec) {
+      let specInfo = {
+        id: resSpec.id,
+        product_name: resSpec.product_name,
+        purchase_price: resSpec.purchase_price,
+        relevance_code: resSpec.relevance_code,
+        need_num: 1
+      };
+      this.onSpecSelected([specInfo]);
+      this.dialogSpecVisible = false;
+    },
+    onBarcodeSubmit() {
+      $http
+        .getProductsByScan({
+          barcode: this.barcode
+        })
+        .then(res => {
+          if (res.data.specs.length === 1) {
+            res.data.specs[0].product_name = res.data.name_cn + res.data.specs[0].name_cn;
+            this.confirmBarcodeSubmit(res.data.specs[0]);
+            console.log('created');
+          } else {
+            res.data.specs.forEach(item => {
+              item.product_name = res.data.name_cn + item.name_cn;
+            });
+            this.barcodeSearchSpecsResult = res.data.specs;
+            this.dialogSpecVisible = true;
+          }
+        });
+    },
     handlerDistributorListClose() {
       this.queryAllDistributor();
       for (let i = 0; i < this.distributorSelectList.length; i += 1) {
@@ -417,6 +496,9 @@ export default {
     onSpecSelected(data) {
       for (let i = 0; i < data.length; i += 1) {
         let found = false;
+        if (isNaN(data[i].need_num)) {
+          data[i].need_num = 1;
+        }
         for (let j = 0; j < this.specList.length; j += 1) {
           if (data[i].id === this.specList[j].id) {
             found = true;
